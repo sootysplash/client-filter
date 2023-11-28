4f65a4dca6d9afddec68a1f35a7e4bedbfebc9cc
jeffrey <sootysplash1@gmail.com>
2023-11-28 10:02:43 +0400

    cancelling the chat msg instead of sending an empty one

--- src/client/java/me/sootysplash/mixin/client/ExampleClientMixin.java
+++ src/client/java/me/sootysplash/mixin/client/ExampleClientMixin.java
@@ -7,7 +7,9 @@ import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientPlayNetworkHandler.class)
 public class ExampleClientMixin {
@@ -16,10 +18,9 @@ public class ExampleClientMixin {
 	boolean badwordsaid = false;
 	@Unique
 	String[] words = new String[]{"tranny", "nigger", "chink", "nigga", "faggot", "fag", "kys", "kill yourself", "kill urself", "you're dogshit", "your dogshit", "ur dogshit", "testword123"};
-	@ModifyVariable(method = "sendChatMessage", at = @At("HEAD"), ordinal = 0, argsOnly = true)
-	private String fabric_modifySendChatMessage(String content) {
-		content = ClientSendMessageEvents.MODIFY_CHAT.invoker().modifySendChatMessage(content);
-		ClientSendMessageEvents.CHAT.invoker().onSendChatMessage(content);
+
+	@Inject(method = "sendChatMessage", at = @At("HEAD"), cancellable = true)
+	private void fabric_allowSendChatMessage(String content, CallbackInfo ci) {
 
 		for(int i = 0; i < words.length; i++) {
 			if (content.contains(words[i])) {
@@ -28,22 +29,18 @@ public class ExampleClientMixin {
 			}
 			i++;
 		}
+
 		if(badwordsaid){
 			content = "";
 			MinecraftClient.getInstance().inGameHud.getChatHud().addMessage(Text.of("[ChatFilter] Hey buddy, offensive words aren't the right way to release your anger."));
 			badwordsaid = false;
+			ClientSendMessageEvents.CHAT_CANCELED.invoker().onSendChatMessageCanceled(content);
+			ci.cancel();
 		}
 
-
-		return content;
 	}
-
-	@ModifyVariable(method = "sendChatCommand", at = @At("HEAD"), ordinal = 0, argsOnly = true)
-	private String fabric_modifySendCommandMessage(String command) {
-		command = ClientSendMessageEvents.MODIFY_COMMAND.invoker().modifySendCommandMessage(command);
-		ClientSendMessageEvents.COMMAND.invoker().onSendCommandMessage(command);
-
-
+	@Inject(method = "sendChatCommand", at = @At("HEAD"), cancellable = true)
+	private void fabric_allowSendCommandMessage(String command, CallbackInfo ci) {
 
 		for(int i = 0; i < words.length; i++) {
 			if (command.contains(words[i])) {
@@ -52,12 +49,15 @@ public class ExampleClientMixin {
 			}
 			i++;
 		}
+
 		if(badwordsaid){
 			command = "";
 			MinecraftClient.getInstance().inGameHud.getChatHud().addMessage(Text.of("[ChatFilter] Hey buddy, offensive words aren't the right way to release your anger."));
 			badwordsaid = false;
+			ClientSendMessageEvents.COMMAND_CANCELED.invoker().onSendCommandMessageCanceled(command);
+			ci.cancel();
 		}
 
-		return command;
 	}
+
 }
